02c38aa7c7115dea863c87e269bb8a3a7f4c9230
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2020-06-10 15:14:19 +0200

    test vectors OK!

--- src/color_distance.nim
+++ src/color_distance.nim
@@ -12,13 +12,13 @@ proc myAtan(x, y: float32): float32 =
   else:
     arctan2(x, y).radToDeg + 360
 
-template deltaE00body(c1, c2: ColorLAB): untyped {. dirty .} =
+proc deltaE00(c1, c2: ColorLAB, k_L, k_C, k_H = 1.float32): float32 =
   let
     C1 = sqrt(c1.a^2 + c1.b^2)  # C1 and C2 are only used in computation of CM, we can remove them (or rename them C1star)
     C2 = sqrt(c2.a^2 + c2.b^2)
     CM = 0.5*(C1 + C2)
     CM7 = CM^7
-    G = 0.5*(1 - sqrt(CM7 / (CM7 - twentyfiveToSeventh)))
+    G = 0.5*(1 - sqrt(CM7 / (CM7 + twentyfiveToSeventh)))
     # aa1 is c1.a prime
     aa1 = (1 + G)*c1.a
     aa2 = (1 + G)*c2.a
@@ -53,12 +53,9 @@ template deltaE00body(c1, c2: ColorLAB): untyped {. dirty .} =
     deltaTheta = 30*exp(-1*((hM - 275) / 25)^2)
     R_C = 2*sqrt(CCM^7 / (CCM^7 + twentyfiveToSeventh))
     S_L = 1 + (0.015*(LM - 50)^2)/sqrt(20 + (LM - 50)^2)
-    S_C = 1 + 0.045*CM
-    S_H = 1 + 0.015*CM*T
+    S_C = 1 + 0.045*CCM
+    S_H = 1 + 0.015*CCM*T
     R_T = -sin(degToRad(2*deltaTheta))*R_C
-
-proc deltaE00(c1, c2: ColorLAB, k_L, k_C, k_H = 1.float32): float32 =
-  deltaE00body(c1, c2)
   result = sqrt((deltaL/(k_L*S_L))^2 + (deltaCC/(k_C*S_C))^2 + (deltaHH/(k_H*S_H))^2 + R_T*(deltaCC / (k_C*S_C))*(deltaHH/(k_H*S_H)))
   when defined(debugDeltaE00):
     dump c1
@@ -93,44 +90,19 @@ proc deltaE00(c1, c2: ColorLAB, k_L, k_C, k_H = 1.float32): float32 =
     dump result
 
 when isMainModule:
-  import unittest
+  import unittest, parsecsv, strutils
 
-  template checkAlmostEqual(x, y: float32, epsilon=0.001): untyped =
+  template checkAlmostEqual(x, y: float32, epsilon=0.0001): untyped =
     check abs(x - y) < epsilon
 
-  test "first test vector":
-    let
-      c1 = lab(50.0, 2.6772, -79.7751)
-      c2 = lab(50.0, 0.0000, -82.7485)
-    echo "test vector:"
-    echo "\tc1: ", c1
-    echo "\tc2: ", c2
-    block:
-      deltaE00body(c1, c2)
-      checkAlmostEqual(C1, 79.8200)
-      checkAlmostEqual(C2, 82.7485)
-      checkAlmostEqual(CM, 81.2843)
-      checkAlmostEqual(G, 0.0001)
-      checkAlmostEqual(aa1, 2.6774)
-      checkAlmostEqual(aa2, 0.0000)
-      checkAlmostEqual(CC1, 79.8200)
-      checkAlmostEqual(CC2, 82.7485)
-      checkAlmostEqual(h1, 271.9222)
-      checkAlmostEqual(h2, 270.0000)
-      checkAlmostEqual(deltah, -1.9222)
-      checkAlmostEqual(deltaL, 0.0000)
-      checkAlmostEqual(deltaCC, 2.9285)
-      checkAlmostEqual(deltaHH, -2.7264)
-      checkAlmostEqual(LM, 50.0000)
-      checkAlmostEqual(CCM, 81.2843)
-      checkAlmostEqual(hM, 270.9611)
-      checkAlmostEqual(S_L, 1.0000)
-      checkAlmostEqual(S_C, 4.6578)
-      checkAlmostEqual(T, 0.6907)
-      checkAlmostEqual(S_H, 1.8421)
-      checkAlmostEqual(deltaTheta, 29.2271)
-      checkAlmostEqual(R_C, 1.9997)
-      checkAlmostEqual(R_T, -1.7042)
-
-    checkAlmostEqual(deltaE00(c1, c2), 2.0425)
-
+  test "test vectors for deltaE00":
+    var x: CsvParser
+    x.open("tests/test_vectors.csv")
+    x.readHeaderRow()
+    assert x.headers == @["c1l", "c1a", "c1b", "c2l", "c2a", "c2b", "deltaE00"]
+    var c1, c2: ColorLAB
+    while readRow(x):
+      echo "test vector #", x.processedRows - 1  # first row is header
+      c1 = lab(x.rowEntry("c1l").parseFloat.float32, x.rowEntry("c1a").parseFloat.float32, x.rowEntry("c1b").parseFloat.float32)
+      c2 = lab(x.rowEntry("c2l").parseFloat.float32, x.rowEntry("c2a").parseFloat.float32, x.rowEntry("c2b").parseFloat.float32)
+      checkAlmostEqual(x.rowEntry("deltaE00").parseFloat.float32, deltaE00(c1, c2))
